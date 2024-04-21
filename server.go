e855d77345ef45703cd80267ba15237cfb116fc1
Umesh Pawar <umesh.a.pawar2002@gmail.com>
2024-04-21 19:57:17 +0530

    initial commit, add air hot reloading

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	fmt.Println("Welcome to auth-svc")
+
+	app := fiber.New()
+
+	error := app.Listen(":3000")
+	if error != nil {
+		fmt.Println("error starting server : ", error)
+	} else {
+		fmt.Println("listing on port 6000")
+	}
+}
